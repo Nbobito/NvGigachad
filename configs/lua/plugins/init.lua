da30aa6b50d203ef0a42d03e2c4fb5f89d002f17
Nbobito <nbobito12@gmail.com>
2024-06-02 21:58:18 -0600

    Test

--- configs/lua/plugins/init.lua
+++ configs/lua/plugins/init.lua
@@ -249,7 +249,7 @@ return {
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
       vim.g.vimtex_view_method = "zathura"
-      vim.g.tex_flavor = "latex"
+      --vim.g.tex_flavor = "latex"
       vim.g.vimtex_quickfix_mode = 2
       vim.g.conceallevel = 2
     end,
